eb1ffe234bd9a39e74e342669e0c83343881ec47
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-10-22 00:24:51 -0500

    feat(utils): add calculate method to calculate a formula in a text.

---
+++ src/main/java/me/bryang/labority/utils/TextUtils.java
@@ -0,0 +1,16 @@
+package me.bryang.labority.utils;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+public class TextUtils {
+
+    public static int calculateNumber(String format, int level){
+        Expression expression = new ExpressionBuilder(format)
+                .variables("%j")
+                .build()
+                .setVariable("%j", level);
+
+        return (int) expression.evaluate();
+    }
+}
