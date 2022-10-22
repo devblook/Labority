5a78f1beec5b3f1164dca255a3f0df2f6a2ead88
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-10-22 09:54:33 -0500

    feat(work): add gain xp method and increment level method

--- src/main/java/me/bryang/labority/listener/plugin/JobListener.java
+++ src/main/java/me/bryang/labority/listener/plugin/JobListener.java
@@ -2,21 +2,34 @@ package me.bryang.labority.listener.plugin;
 
 import me.bryang.labority.PluginCore;
 import me.bryang.labority.action.Action;
+import me.bryang.labority.data.JobData;
+import me.bryang.labority.data.PlayerData;
 import me.bryang.labority.events.JobsEvent;
+import me.bryang.labority.loader.DataLoader;
 import me.bryang.labority.manager.file.FileManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class JobListener implements Listener {
 
     private final FileManager configFile;
+    private final FileManager messagesFile;
+
+    private final DataLoader dataLoader;
 
     public JobListener(PluginCore pluginCore){
+
         this.configFile = pluginCore.getFilesLoader().getConfigFile();
+        this.messagesFile = pluginCore.getFilesLoader().getMessagesFile();
+
+        this.dataLoader = pluginCore.getDataLoader();
     }
 
     @EventHandler
     public void onWork(JobsEvent event){
+
         for (String jobs : configFile.getConfigurationSection("jobs").getKeys(false)){
 
             Action action = event.getAction();
@@ -33,6 +46,26 @@ public class JobListener implements Listener {
 
             if (configFile.getString("jobs." + jobs + ".items." + dataRequired) == null){
                 continue;
+            }
+
+            JobData jobData = dataLoader.getPlayerJob(event.getTarget()).getJob(jobs);
+
+            if (jobData == null){
+                return;
+            }
+
+            String[] jobValue = configFile.getString("jobs." + jobs + ".items." + dataRequired).split(",");
+
+            jobData.setXPPoints(Integer.parseInt(jobValue[1]));
+
+            if (jobData.getMaxXP() <= jobData.getXpPoints()) {
+                jobData.setLevel(jobData.getLevel() + 1);
+                jobData.setXPPoints(jobData.getMaxXP() - jobData.getXpPoints());
+
+                Player player = Bukkit.getPlayer(event.getTarget());
+
+                player.sendMessage(messagesFile.getString("jobs.gain.level")
+                        .replace("%new_level", String.valueOf(jobData.getLevel())));
             }
 
 
