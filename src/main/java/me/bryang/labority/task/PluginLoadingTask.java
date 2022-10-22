01b5d80c5f17ff6937c426baa9b4d651e64560d7
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-10-22 00:32:33 -0500

    feat(task): add max-xp setter method in plugin loading task

--- src/main/java/me/bryang/labority/task/PluginLoadingTask.java
+++ src/main/java/me/bryang/labority/task/PluginLoadingTask.java
@@ -6,18 +6,26 @@ import me.bryang.labority.data.PlayerData;
 import me.bryang.labority.loader.DataLoader;
 import me.bryang.labority.manager.file.FileDataManager;
 import me.bryang.labority.manager.file.FileManager;
+import me.bryang.labority.utils.TextUtils;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
 public class PluginLoadingTask {
 
+    private final FileManager configFile;
     private final FileDataManager playersFile;
+
+
     private final DataLoader dataLoader;
+
 
     public PluginLoadingTask(PluginCore pluginCore){
 
+        this.configFile = pluginCore.getFilesLoader().getConfigFile();
         this.playersFile = pluginCore.getFilesLoader().getPlayersFile();
+
+
         this.dataLoader = pluginCore.getDataLoader();
 
     }
@@ -37,6 +45,7 @@ public class PluginLoadingTask {
 
                 jobData.setLevel(playersFile.getJobData(playerUniqueId).getInt(".list-jobs." + jobName + ".xp"));
                 jobData.setLevel(playersFile.getJobData(playerUniqueId).getInt(".list-jobs." + jobName + ".level"));
+                playerData.getJob(jobName).setMaxXP(TextUtils.calculateNumber(configFile.getString("jobs.formula.max-xp"), 1));
 
                 playerData.addJobData(jobName, jobData);
             }
