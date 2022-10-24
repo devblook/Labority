586ec4a13c3d50f8cd81296cc78e8e3abafd12b4
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-10-23 22:10:05 -0500

    feat(block-kill-entity-listener): add disable condition

--- src/main/java/me/bryang/labority/listener/PlayerKillEntityListener.java
+++ src/main/java/me/bryang/labority/listener/PlayerKillEntityListener.java
@@ -1,9 +1,12 @@
 package me.bryang.labority.listener;
 
+import me.bryang.labority.PluginCore;
 import me.bryang.labority.action.Action;
 import me.bryang.labority.action.JobType;
 import me.bryang.labority.events.JobsEvent;
+import me.bryang.labority.manager.file.FileManager;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,11 +15,23 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class PlayerKillEntityListener implements Listener {
 
+    private final FileManager configFile;
+
+    public PlayerKillEntityListener(PluginCore pluginCore){
+        configFile = pluginCore.getFilesLoader().getConfigFile();
+    }
+
     @EventHandler
     public void PlayerKillMob(EntityDamageByEntityEvent event) {
 
         if (event.getDamager().getType() != EntityType.PLAYER) {
             return;
+        }
+
+        if (((Player) event.getDamager()).getGameMode() == GameMode.ADVENTURE) {
+            if (configFile.getBoolean("config.disable-creative")) {
+                return;
+            }
         }
 
         Player player = (Player) event.getDamager();
