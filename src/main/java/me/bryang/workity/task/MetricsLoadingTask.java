b97340f7ffcc5f2e8bc2332b70fbe5d5d752d496
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-10-25 11:25:52 -0500

    feat(metric-loading-task): create task

---
+++ src/main/java/me/bryang/workity/task/MetricsLoadingTask.java
@@ -0,0 +1,23 @@
+package me.bryang.workity.task;
+
+import me.bryang.workity.PluginCore;
+import me.bryang.workity.PluginMetrics;
+import me.bryang.workity.Workity;
+import me.bryang.workity.manager.file.FileManager;
+
+public class MetricsLoadingTask {
+
+    private final Workity workity;
+    private final FileManager configFile;
+
+    public MetricsLoadingTask(PluginCore pluginCore){
+        this.workity = pluginCore.getPlugin();
+        this.configFile = pluginCore.getFilesLoader().getConfigFile();
+    }
+
+    public void loadTask(){
+        if (configFile.getBoolean("enabled-metrics")) {
+            PluginMetrics pluginMetrics = new PluginMetrics(workity, 16726);
+        }
+    }
+}
