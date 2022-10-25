c5d5514b5d8c005a74c7bf95c70562ab8c61c436
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-10-25 11:24:23 -0500

    feat(task-manager): add metric-loading-task

--- src/main/java/me/bryang/workity/manager/TaskManager.java
+++ src/main/java/me/bryang/workity/manager/TaskManager.java
@@ -1,6 +1,7 @@
 package me.bryang.workity.manager;
 
 import me.bryang.workity.PluginCore;
+import me.bryang.workity.task.MetricsLoadingTask;
 import me.bryang.workity.task.PluginLoadingTask;
 
 public class TaskManager {
@@ -8,6 +9,7 @@ public class TaskManager {
     private final PluginCore pluginCore;
 
     private PluginLoadingTask pluginLoadingTask;
+    private MetricsLoadingTask metricsLoadingTask;
 
     public TaskManager(PluginCore pluginCore) {
         this.pluginCore = pluginCore;
@@ -15,11 +17,14 @@ public class TaskManager {
 
     public void load() {
         pluginLoadingTask = new PluginLoadingTask(pluginCore);
-
+        metricsLoadingTask = new MetricsLoadingTask(pluginCore);
     }
 
     public PluginLoadingTask getPluginLoadingTask() {
         return pluginLoadingTask;
     }
 
+    public MetricsLoadingTask getMetricsLoadingTask() {
+        return metricsLoadingTask;
+    }
 }
