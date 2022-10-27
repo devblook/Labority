4c5589f23a41c4343ed1dab77959dbd0c2569c1d
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-10-27 14:07:59 -0500

    feat(jobs-command): put main sub command

--- src/main/java/me/bryang/workity/commands/JobsCommand.java
+++ src/main/java/me/bryang/workity/commands/JobsCommand.java
@@ -2,9 +2,14 @@ package me.bryang.workity.commands;
 
 import me.bryang.workity.PluginCore;
 import me.bryang.workity.commands.subcommands.*;
+import me.bryang.workity.manager.file.FileManager;
 import me.fixeddev.commandflow.annotated.CommandClass;
 import me.fixeddev.commandflow.annotated.annotation.Command;
 import me.fixeddev.commandflow.annotated.annotation.SubCommandClasses;
+import me.fixeddev.commandflow.bukkit.annotation.Sender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
 
 @Command(
 
@@ -12,7 +17,6 @@ import me.fixeddev.commandflow.annotated.annotation.SubCommandClasses;
         desc = "Main command.")
 
 @SubCommandClasses({
-        MainSubCommand.class,
         AddLevelSubCommand.class,
         BrowseSubCommand.class,
         HelpSubCommand.class,
@@ -29,9 +33,36 @@ import me.fixeddev.commandflow.annotated.annotation.SubCommandClasses;
 public class JobsCommand implements CommandClass {
 
     private final PluginCore pluginCore;
+    private final FileManager messagesFile;
 
     public JobsCommand(PluginCore pluginCore) {
         this.pluginCore = pluginCore;
+        this.messagesFile = pluginCore.getFilesLoader().getMessagesFile();
+    }
+
+    @Command(
+            names = "")
+
+    public boolean onMainSubCommand(
+
+            @Sender Player sender) {
+
+        List<String> helpCommandList = messagesFile.getStringList("jobs.help.format");
+
+        for (String message : helpCommandList) {
+            sender.sendMessage(message);
+        }
+
+        if (!sender.hasPermission("jobs.admin")) {
+            return true;
+        }
+
+        List<String> helpCommandAdmin = messagesFile.getStringList("jobs.help.admin");
+
+        for (String message : helpCommandAdmin) {
+            sender.sendMessage(message);
+        }
+        return true;
     }
 
     public PluginCore getPluginCore() {
