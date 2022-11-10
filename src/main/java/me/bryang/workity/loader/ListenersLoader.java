c86c66b28a490d4e537b4e718e020fbda10f3822
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-11-09 20:10:07 -0500

    refactor(listeners-loader): change package name

--- src/main/java/me/bryang/workity/loader/ListenersLoader.java
+++ src/main/java/me/bryang/workity/loader/ListenersLoader.java
@@ -3,7 +3,7 @@ package me.bryang.workity.loader;
 import me.bryang.workity.PluginCore;
 import me.bryang.workity.Workity;
 import me.bryang.workity.interfaces.Loader;
-import me.bryang.workity.listener.*;
+import me.bryang.workity.listeners.*;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
