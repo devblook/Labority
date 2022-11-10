a758faae835967a4dc84f3dd0daab156e45067a8
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-11-09 20:04:47 -0500

    feat(work-action-manager): create class

---
+++ src/main/java/me/bryang/workity/manager/WorkActionManager.java
@@ -0,0 +1,41 @@
+package me.bryang.workity.manager;
+
+import me.bryang.workity.PluginCore;
+import me.bryang.workity.data.PlayerJobData;
+import me.bryang.workity.manager.jobs.*;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WorkActionManager {
+
+    private final PluginCore pluginCore;
+
+    private final List<WorkAction> workActions = new ArrayList<>();
+
+    public WorkActionManager(PluginCore pluginCore) {
+        this.pluginCore = pluginCore;
+    }
+
+    public void loadActions() {
+        addActions(
+                new JobSetMultiplierManager(pluginCore),
+                new JobGainRewardManager(pluginCore),
+                new JobIncreaseLevelManager(pluginCore),
+                new JobAddStatsPointsManager(pluginCore),
+                new JobActionRewardsManager(pluginCore));
+
+    }
+
+    public void addActions(WorkAction... newWorkActions) {
+        workActions.addAll(Arrays.asList(newWorkActions));
+    }
+
+    public void doActions(Player player, String jobName, String itemName, PlayerJobData playerJobData) {
+        for (WorkAction workAction : workActions) {
+            workAction.doWorkAction(player, jobName, itemName, playerJobData);
+        }
+    }
+}
