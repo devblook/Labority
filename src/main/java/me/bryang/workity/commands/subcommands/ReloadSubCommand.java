1507c8b7ab52e270a177a57819870921e3948fac
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-11-09 20:13:27 -0500

    feat(reload-sub-command): add job-data-map and activities

--- src/main/java/me/bryang/workity/commands/subcommands/ReloadSubCommand.java
+++ src/main/java/me/bryang/workity/commands/subcommands/ReloadSubCommand.java
@@ -1,22 +1,32 @@
 package me.bryang.workity.commands.subcommands;
 
+import me.bryang.workity.activites.JobsLoadingActivities;
 import me.bryang.workity.commands.JobsCommand;
+import me.bryang.workity.data.job.JobData;
 import me.bryang.workity.manager.file.FileManager;
 import me.fixeddev.commandflow.annotated.CommandClass;
 import me.fixeddev.commandflow.annotated.annotation.Command;
 import me.fixeddev.commandflow.bukkit.annotation.Sender;
 import org.bukkit.entity.Player;
 
+import java.util.Map;
+
 public class ReloadSubCommand implements CommandClass {
 
     private final FileManager configFile;
     private final FileManager messagesFile;
+
+    private final Map<String, JobData> jobDataMap;
+    private final JobsLoadingActivities jobsLoadingActivities;
 
 
     public ReloadSubCommand(JobsCommand jobsCommand) {
         this.messagesFile = jobsCommand.getPluginCore().getFilesLoader().getMessagesFile();
         this.configFile = jobsCommand.getPluginCore().getFilesLoader().getConfigFile();
 
+        this.jobDataMap = jobsCommand.getPluginCore().getDataLoader().getJobDataMap();
+        this.jobsLoadingActivities = jobsCommand.getPluginCore().getManagerLoader()
+                .getTaskManager().getJobsLoadingActivities();
     }
 
     @Command(
@@ -29,6 +39,10 @@ public class ReloadSubCommand implements CommandClass {
 
         configFile.reload();
         messagesFile.reload();
+
+        jobDataMap.clear();
+        jobsLoadingActivities.loadTask();
+
         sender.sendMessage(messagesFile.getString("jobs.reload"));
         return true;
     }
