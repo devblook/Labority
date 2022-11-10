305217320f64b2b63a75011fe8d4d68d0573d3ce
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-11-09 19:18:17 -0500

    refactor(metrics-loading-activities): change name class

--- src/main/java/me/bryang/workity/activites/MetricsLoadingActivities.java
+++ src/main/java/me/bryang/workity/activites/MetricsLoadingActivities.java
@@ -1,16 +1,17 @@
-package me.bryang.workity.task;
+package me.bryang.workity.activites;
 
 import me.bryang.workity.PluginCore;
 import me.bryang.workity.PluginMetrics;
 import me.bryang.workity.Workity;
+import me.bryang.workity.interfaces.Activities;
 import me.bryang.workity.manager.file.FileManager;
 
-public class MetricsLoadingTask {
+public class MetricsLoadingActivities implements Activities {
 
     private final Workity workity;
     private final FileManager configFile;
 
-    public MetricsLoadingTask(PluginCore pluginCore) {
+    public MetricsLoadingActivities(PluginCore pluginCore) {
         this.workity = pluginCore.getPlugin();
         this.configFile = pluginCore.getFilesLoader().getConfigFile();
     }
