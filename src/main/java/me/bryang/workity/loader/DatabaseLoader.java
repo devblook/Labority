694553bc4fdb7e8c8c48d6d3edcfce37bb721021
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-11-16 14:31:10 -0500

    feat(data-base-loader): create data-base class to load the databases

---
+++ src/main/java/me/bryang/workity/loader/DatabaseLoader.java
@@ -0,0 +1,33 @@
+package me.bryang.workity.loader;
+
+import me.bryang.workity.PluginCore;
+import me.bryang.workity.Workity;
+import me.bryang.workity.database.Database;
+import me.bryang.workity.database.PlayerFilesDatabase;
+import me.bryang.workity.manager.file.FileManager;
+
+public class DatabaseLoader implements Loader{
+
+    private final Workity workity;
+
+    private final FileManager configFile;
+    private Database database;
+
+    public DatabaseLoader(PluginCore pluginCore) {
+        this.workity = pluginCore.getPlugin();
+        this.configFile = pluginCore.getFilesLoader().getConfigFile();
+    }
+
+
+    @Override
+    public void load() {
+
+        if (configFile.getString("database.type").equalsIgnoreCase("NONE")){
+            database = new PlayerFilesDatabase(workity);
+        }
+    }
+
+    public Database getDatabase(){
+        return database;
+    }
+}
