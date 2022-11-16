2af41be063b1c68469dedbd82556d213e7a08022
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-11-16 14:31:45 -0500

    feat(database): create data-base model

---
+++ src/main/java/me/bryang/workity/database/Database.java
@@ -0,0 +1,29 @@
+package me.bryang.workity.database;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface Database {
+
+    Database insertJobData(UUID playerUniqueId, String jobName, String playerData, String newData);
+    Database insertJobData(UUID playerUniqueId, String jobName, String playerData, int newData);
+    Database insertJobData(UUID playerUniqueId, String jobName, String playerData, double newData);
+
+    void insertData(UUID playerUniqueId, String playerData, int newData);
+
+    String getJobStringData(UUID playerUniqueId, String jobName, String playerData);
+    int getJobIntData(UUID playerUniqueId, String jobName, String playerData);
+    double getJobDoubleData(UUID playerUniqueId, String jobName, String playerData);
+
+    String getStringData(UUID playerUniqueId, String playerData);
+    int getIntData(UUID playerUniqueId, String playerData);
+    double getDoubleData(UUID playerUniqueId, String playerData);
+
+
+    List<String> getPlayerList();
+    List<String> getPlayerJobs(UUID playerUniqueId);
+    void save();
+
+
+
+}
