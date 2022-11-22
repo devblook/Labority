71010744079a3fee1caf8c815508348fccb7bf86
BryanG <47282239+BryanGamingDV@users.noreply.github.com>
2022-11-22 16:08:30 -0500

    style: optimize imports

--- src/main/java/me/bryang/workity/listeners/WorkCraftListener.java
+++ src/main/java/me/bryang/workity/listeners/WorkCraftListener.java
@@ -1,17 +1,12 @@
 package me.bryang.workity.listeners;
 
-import me.bryang.workity.PluginCore;
 import me.bryang.workity.action.JobAction;
 import me.bryang.workity.action.JobType;
 import me.bryang.workity.events.JobExecuteEvent;
-import me.bryang.workity.manager.file.FileManager;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class WorkCraftListener implements Listener {
 
